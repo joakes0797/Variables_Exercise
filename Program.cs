138e188a64050a7c408759992ef931fba8706e01
Jill Oakes <jericho_rules@hotmail.com>
2022-03-18 12:51:23 -0400

    Initialize Variables_Exercise

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Variables_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string timeLine = "score and";
+            int timeYears = 7;
+            char timeLetter = 'a';
+            bool isAlive = true;
+            double myScore = 99.99;
+            decimal myAge = 21;
+
+            Console.WriteLine($"Four {timeLine} {timeYears} years ago {timeLetter} {isAlive}Coder student scored {myScore} percent on {myAge} quizzes.");
+        }
+    }
+}
